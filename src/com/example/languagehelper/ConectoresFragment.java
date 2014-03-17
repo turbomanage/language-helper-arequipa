e9ecfa0b355257c42866f91c8d92c5178f8b2f06
David M. Chandler <turbomanage@gmail.com>
2014-03-17 15:02:59 -0500

    Load word files from assets instead of res

--- src/com/example/languagehelper/ConectoresFragment.java
+++ src/com/example/languagehelper/ConectoresFragment.java
@@ -1,18 +1,22 @@
 package com.example.languagehelper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 
 public class ConectoresFragment extends ListFragment {
-	
+
 	private WordAdapter wordAdapter;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
-		String[] originales = this.getActivity().getResources()
-				.getStringArray(R.array.conectores_originales);
-		String[] traducciones = this.getActivity().getResources()
-				.getStringArray(R.array.conectores);
+		String[] originales = readWords("originales.txt");
+		String[] traducciones = readWords("palabras-en.txt");
 		Palabra[] palabras = new Palabra[originales.length];
 		for (int i = 0; i < originales.length; i++) {
 			palabras[i] = new Palabra(originales[i], traducciones[i]);
@@ -22,8 +26,27 @@ public class ConectoresFragment extends ListFragment {
 		super.onActivityCreated(savedInstanceState);
 	}
 
+	private String[] readWords(String filename) {
+		ArrayList<String> words = new ArrayList<String>();
+		try {
+			InputStream is = getResources().getAssets().open(filename);
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader bufferedReader = new BufferedReader(isr);
+			// primera pasada atrás del ciclo
+			String word = bufferedReader.readLine();
+			while (word != null) {
+				words.add(word);
+				word = bufferedReader.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return words.toArray(new String[words.size()]);
+	}
+
 	public void swapViews() {
 		wordAdapter.swapViews();
 	}
-	
+
 }
